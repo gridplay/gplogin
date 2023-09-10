0910f09cbf07f021f8c48fa97dd71a867ecab391
Ven Hansome <42349180+Chrisx84@users.noreply.github.com>
2023-09-10 12:12:25 -0400

    Update Provider.php

--- Provider.php
+++ Provider.php
@@ -4,7 +4,7 @@ use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
 class Provider extends AbstractProvider {
     public const IDENTIFIER = 'gplogin';
-    protected $scopes = ['users'];
+    protected $scopes = ['*'];
     protected $scopeSeparator = ' ';
     protected function getAuthUrl($state) {
         return $this->buildAuthUrlFromBase(
