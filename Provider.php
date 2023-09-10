3b4c4bcd83bf75d6597787d7e9c978255bdfbae5
Ven Hansome <42349180+Chrisx84@users.noreply.github.com>
2023-09-10 12:27:56 -0400

    rework
    hope this works

--- Provider.php
+++ Provider.php
@@ -1,46 +1,110 @@
 <?php
+
 namespace GPLogin;
+
+use GuzzleHttp\RequestOptions;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
-class Provider extends AbstractProvider {
-    public const IDENTIFIER = 'gplogin';
+
+class Provider extends AbstractProvider
+{
+    public const IDENTIFIER = 'GPLOGIN';
+
+    /**
+     * {@inheritdoc}
+     */
     protected $scopes = ['users'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $consent = false;
+
+    /**
+     * {@inheritdoc}
+     */
     protected $scopeSeparator = ' ';
-    protected function getAuthUrl($state) {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
         return $this->buildAuthUrlFromBase(
-            'https://gridplay.net/oauth/authorize',
+            'https://gridplay.net/oauth2/authorize',
             $state
         );
     }
-    protected function getTokenUrl() {
-        return 'https://gridplay.net/oauth/token';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCodeFields($state = null)
+    {
+        $fields = parent::getCodeFields($state);
+
+        if (! $this->consent) {
+            $fields['prompt'] = 'none';
+        }
+
+        return $fields;
     }
-    protected function getUserByToken($token) {
+
+    /**
+     * Prompt for consent each time or not.
+     *
+     * @return $this
+     */
+    public function withConsent()
+    {
+        $this->consent = true;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://gridplay.net/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
         $response = $this->getHttpClient()->get(
             'https://gridplay.net/api/users',
             [
-                'headers' => [
+                RequestOptions::HEADERS => [
                     'Authorization' => 'Bearer '.$token,
                 ],
             ]
         );
 
-        return json_decode($response->getBody()->getContents(), true);
+        return json_decode((string) $response->getBody(), true);
     }
-    protected function mapUserToObject(array $user) {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
         return (new User())->setRaw($user)->map([
             'id'         => $user['id'],
             'name'       => $user['name'],
-            'email'      => $user['email'],
-            'sl_avatars' => $user['sl_avatars']
+            'email'      => $user['email'] ?? null,
+            'sl_avatars' => $user['sl_avatars'],
         ]);
     }
-    protected function getTokenFields($code) {
-        return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code',
-        ]);
-    }
-    public static function additionalConfigKeys() {
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
         return [];
     }
 }
