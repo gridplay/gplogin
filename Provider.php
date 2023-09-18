d900b6ebe1f08cfe338f19660293669a878617ff
Ven Hansome <42349180+Chrisx84@users.noreply.github.com>
2023-09-18 11:41:40 -0400

    Update Provider.php

--- Provider.php
+++ Provider.php
@@ -8,7 +8,7 @@ use SocialiteProviders\Manager\OAuth2\User;
 
 class Provider extends AbstractProvider
 {
-    public const IDENTIFIER = 'gplogin';
+    public const IDENTIFIER = 'GPLOGIN';
 
     /**
      * {@inheritdoc}
